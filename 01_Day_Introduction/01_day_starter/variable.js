b1992b557d52c786d436cc05ac97565d6e4f5594
akhtar hussain <akhtarhussain0788@gmail.com>
2020-07-26 22:54:13 +0500

    Introducaton

---
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,21 @@
+// Declaring different variables of different data types
+
+let firstName = 'Akhtar'    // first name of a person
+let lastName = 'Hussain'      // last name of a person
+let country = 'Pakistan'       // country
+let city = 'Karachi'         // capital city
+let age = 24                 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+   let name = 'Akhtar',     //name of a person
+  job = 'Joiner Js Developer',
+  live = 'Paksitan'
+
